f764ecfa98f236865502107d453a49ee7c3e23ef
CodeShark-pro <panwar.nihal2004@gmail.com>
2026-03-28 10:56:41 +0530

    Reverted to local mongodb implementation instead of atlas

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import httpx
-import certifi 
+
 load_dotenv()
 
 MONGO_DETAILS = os.getenv("MONGO_URI")
 
 if not MONGO_DETAILS:
-    print("⚠️ WARNING: Could not find MONGO_URI in .env file!")
+    print("WARNING: Could not find MONGO_URI in .env file!")
 
-client = AsyncIOMotorClient(MONGO_DETAILS, tlsCAFile=certifi.where()) 
-
+client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.veriguard
 claim_collection = database.claims
+
 # Suppress Windows symlink warnings
 warnings.filterwarnings("ignore")
 
